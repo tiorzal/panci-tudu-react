1e20e6f518962456201cf42f00e62ceb96a65163
tiorzal <tioralmunazir@gmail.com>
2020-12-27 22:17:53 +0700

    update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,7 +9,7 @@ export default class Navbar extends Component {
     if(localStorage.getItem('access_token')){
       button = <button className="btn btn-primary">logout</button>
     } else {
-      button = <button className="btn btn-warning">logout</button>
+      button = <button className="btn btn-warning">login</button>
     }
     return(
       <nav className="navbar navbar-expand-md navbar-light bg-primary">
--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+// import { Link } from 'react-router-dom'
 
 export default class LoginPage extends Component {
   constructor(){
@@ -8,11 +9,45 @@ export default class LoginPage extends Component {
       password: ''
     }
   }
+  
+  login (e) {
+    e.preventDefault()
+    console.log(this.state.email, this.state.password);
+    localStorage.setItem('access_token', this.state.email)
+    document.getElementById("loginForm").reset()
+  }
 
   render() {
     return(
-      <div>
-        ini login page
+      <div className="container">
+        <div className="row">
+          <div className="card col-6 mx-auto p-2" style={{ marginTop: "20vh"}}>
+            <form id="loginForm" className="text-start">
+              <label className="form-label">email</label>
+              <input
+                className="form-control"
+                type="email"
+                onChange={ (e) => {
+                  this.setState({email: e.target.value})
+                }}
+              ></input>
+              <label className="form-label">password</label>
+              <input 
+                className="form-control" 
+                type="password"
+                onChange={ (e) => {
+                  this.setState({password: e.target.value})
+                }}
+              ></input>
+            </form>
+            <div className="card-footer">
+              <button 
+                className="btn btn-primary"
+                onClick={(e)=> this.login(e)}
+              >login</button>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
