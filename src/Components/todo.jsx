b912bebe73ee1a6aaf38af3206d25621ea4dde63
tiorzal <tioralmunazir@gmail.com>
2020-12-29 08:25:39 +0700

    update

--- src/Components/todo.jsx
+++ src/Components/todo.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 
 export default class Todo extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
 
+    }
+  }
   render() {
     return(
       <div>
+        {console.log(this.props)}
         {this.props.todo.title}
       </div>
     )
